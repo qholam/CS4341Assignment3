05892fd815671c9ee1b3eae28169c3ea2c513c71
Nata Vacheishvili <nvacheishvili@wpi.edu>
2017-02-14 18:34:32 -0500

    Create Icy.java

---
+++ CS4341Assignment3/src/Icy.java
@@ -0,0 +1,38 @@
+public class Icy {
+  Humidity humidity;
+  Temperature temp;
+  
+  public double getProbability(){
+  double probability=-1;
+    if(humidity.isLow&&temp.isWarm){
+      probability = 0.001;
+    }
+    else if(humidity.isLow&&temp.isMild){
+      probability = 0.01;
+    }
+    else if(humidity.isLow&&temp.isCold){
+      probability = 0.05;
+    }
+    else if(humidity.isMedium&&temp.isWarm){
+      probability = 0.001;
+    }
+    else if(humidity.isMedium&&temp.isMild){
+      probability = 0.03;
+    }
+    else if(humidity.isMedium&&temp.isCold){
+      probability = 0.2;
+    }
+    else if(humidity.isHigh&&temp.isWarm){
+      probability = 0.005;
+    }
+    else if(humidity.isHigh&&temp.isMild){
+      probability = 0.01;
+    }
+    else if(humidity.isHigh&&temp.isCold){
+      probability=0.35;
+    }
+    return probability;
+  }
+  
+  
+}
