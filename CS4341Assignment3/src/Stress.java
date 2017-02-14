1dd021ccb7f92464791b5befbc6abe79c6698c6e
Nata Vacheishvili <nvacheishvili@wpi.edu>
2017-02-14 18:46:41 -0500

    Create Stress.java

---
+++ CS4341Assignment3/src/Stress.java
@@ -0,0 +1,37 @@
+public Class Stress{
+  Snow snow;
+  Exams exam;
+  boolean isStressed;
+  public Stress(){
+  }
+  public double getProbability(){
+    double probability=-1;
+    if((!snow.isSnow)&&(!exam.isExam)){
+      probability = 0.01;
+    }
+    else if((!snow.isSnow)&&exam.isExam){
+      probability=0.02;
+    }
+    else if(snow.isSnow&&(!exam.isExam)){
+      probability = 0.1;
+    }
+    else if(snow.isSnow&&exam.isExam){
+      probability=0.5;
+    }
+    return probability;
+  }
+  
+  public boolean getIsStressed(){
+    return isStressed;
+  }
+  
+  public void setIsStressed(){
+    int rand = Math.random()*100+1;
+    if(rand<this.getProbability){
+      isStressed=false;
+    }
+    else{
+      isStressed=true;
+    }
+  }
+}
