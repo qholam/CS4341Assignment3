47ca644e5458544e8dcdb6c2c90b547c981b4dd9
nvacheishvili <nvacheishvili@FLA21-07.admin.wpi.edu>
2017-02-14 17:34:06 -0500

    Day and humidity classes

--- CS4341Assignment3/src/Day.java
+++ CS4341Assignment3/src/Day.java
@@ -2,4 +2,5 @@
 public class Day {
 	double weekend = 0.2;
 	double weekday = 0.8;
+	
 }
--- CS4341Assignment3/src/Humidity.java
+++ CS4341Assignment3/src/Humidity.java
@@ -3,4 +3,5 @@ public class Humidity {
 	double low = 0.2;
 	double medium = 0.5;
 	double high = 0.3;
+	
 }
