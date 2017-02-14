f8dafa77ade6e86a9c881dd78c8c60beb087ae71
loriluo95 <zluo2@wpi.edu>
2017-02-14 18:14:59 -0500

    update run sample

---
+++ RunSample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RunSample {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("sample");
+		Scanner scanner = new Scanner(System.in);
+		String[] input = scanner.next().split(" ");
+		for(String s: input)
+		{
+			String[] type = s.split("=");
+			for(String s: type)
+			{
+				System.out.println(s);
+			}
+		}
+	}
+}
