4311db21e483daa135f4eb2b304762e308da09b9
loriluo95 <zluo2@wpi.edu>
2017-02-14 18:54:38 -0500

    update main

--- CS4341Assignment3/src/RunSample.java
+++ CS4341Assignment3/src/RunSample.java
@@ -1,19 +1,74 @@
 package src;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RunSample {
+
+	private static String requestType;
+	private static String requestCondition;
+	private static String examcond;
+	private static String humiditycond;
+	private static String snowcond;
+	private static String icycond;
+	private static String tempcond;
+	private static String daycond;
+	private static String stresscond;
+	private static String cloudycond;
+	private static int iteracond;
+	private static String[] input;
+	private static String[] type;
 
 	public static void main(String[] args) 
 	{
 		System.out.println("sample");
 		Scanner scanner = new Scanner(System.in);
-		String[] input = scanner.next().split(" ");
+		input = scanner.nextLine().split(" ");
+		int i = 0;
 		for(String s: input)
 		{
-			String[] type = s.split("=");
-			for(String s: type)
+			type = s.split("=");
+			for(String str: type)
 			{
-				System.out.println(s);
+				System.out.println(i + " " + str);
+				if(i == 0)
+				{
+					requestType = str;
+				}
+				else if(i == 1)
+				{
+					requestCondition = str;
+				}else if(i == 2)
+				{
+					iteracond = Integer.parseInt(str);
+					System.out.println(iteracond);
+				} else
+				{
+					if(str.toLowerCase().equals("humidity"))
+					{
+						humiditycond = type[i+1];
+					} 
+					else if(str.toLowerCase().equals("day"))
+					{
+						daycond = type[1];
+					}
+					else if(str.toLowerCase().equals("snow"))
+					{
+						snowcond = type[1];
+					}
+					else if(str.toLowerCase().equals("temperature"))
+					{
+						tempcond = type[1];
+					}
+					else if(str.toLowerCase().equals("exam"))
+					{
+						examcond = type[1];
+					}
+					else if(str.toLowerCase().equals("cloudy"))
+					{
+						cloudycond = type[1];
+					}
+				}
+				i++;
 			}
 		}
 	}
